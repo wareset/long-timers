3f799544e59a99020358d4c8cc8a6f0e592ce722
rease <cftcview@gmail.com>
2025-01-03 16:59:20 +0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,62 @@
-let _Timeout = function (_cb: Function, _ms: number, _isRepeat: boolean, _args: any[]) {
+// let _Timeout = function (_cb: Function, _ms: number, _isRepeat: boolean, _args: any[]) {
+
+function loop(iam: Timeout, ms: number) {
   const MAX_TIME = 2e9 // 2e9
+  iam._.t =
+    ms > MAX_TIME
+      ? setTimeout(function () {
+          loop(iam, ms - MAX_TIME)
+        }, MAX_TIME)
+      : setTimeout(function () {
+          if (iam._.i) loop(iam, iam._.m)
+          iam._.f.apply(iam, iam._.a)
+        }, ms)
+  // TODO: What is it??????????????????????
+  iam.hasRef() || iam.unref()
+}
 
-  function loop(iam: Timeout, ms: number) {
-    iam._.t =
-      MAX_TIME < ms
-        ? setTimeout(function () {
-            loop(iam, ms - MAX_TIME)
-          }, MAX_TIME)
-        : setTimeout(function () {
-            if (iam._.i) loop(iam, iam._.m)
-            iam._.f.apply(iam, iam._.a)
-          }, ms)
-    iam.hasRef() || iam.unref()
+class Timeout {
+  declare _: { t: NodeJS.Timeout; f: Function; m: number; i: boolean; a: any[]; r: boolean }
+
+  constructor(cb: Function, ms: number | undefined, isRep: boolean, args: any[]) {
+    this._ = {
+      t: 0 as any,
+      f: cb,
+      m: (ms = (ms = +ms!) > 0 ? ms : 1),
+      i: isRep,
+      a: args,
+      r: true,
+    }
+    loop(this, ms)
   }
 
-  class Timeout {
-    declare _: { t: NodeJS.Timeout; f: Function; m: number; i: boolean; a: any[]; r: boolean }
-
-    constructor(cb: Function, ms: number, isRep: boolean, args: any[]) {
-      this._ = { t: 0 as any, f: cb, m: (ms = (ms = +ms) > 0 ? ms : 1), i: isRep, a: args, r: true }
-      loop(this, ms)
-    }
-
-    close() {
-      return clearTimeout(this._.t), this
-    }
-
-    refresh() {
-      return clearTimeout(this._.t), loop(this, this._.m), this
-    }
-
-    hasRef() {
-      const _ = this._
-      return _.t.hasRef ? (_.r = _.t.hasRef()) : _.r
-    }
-
-    ref() {
-      const _ = this._
-      return _.r || (_.t.ref && _.t.ref()), (_.r = true), this
-    }
-
-    unref() {
-      const _ = this._
-      return _.r && _.t.unref && _.t.unref(), (_.r = false), this
-    }
+  close() {
+    return clearTimeout(this._.t), this
   }
 
-  _Timeout = Timeout
-  return new _Timeout(_cb, _ms, _isRepeat, _args)
-} as any
+  refresh() {
+    return clearTimeout(this._.t), loop(this, this._.m), this
+  }
+
+  hasRef() {
+    const _ = this._
+    return _.t.hasRef ? (_.r = _.t.hasRef()) : _.r
+  }
+
+  ref() {
+    const _ = this._
+    return _.r || (_.t.ref && _.t.ref()), (_.r = true), this
+  }
+
+  unref() {
+    const _ = this._
+    return _.r && _.t.unref && _.t.unref(), (_.r = false), this
+  }
+}
+
+//   _Timeout = Timeout
+//   return new _Timeout(_cb, _ms, _isRepeat, _args)
+// } as any
 
 export declare class ITimeout {
   close(): this
@@ -63,7 +71,7 @@ function longSetTimeout<F extends (...a: any[]) => any>(
   ms?: number,
   ...args: Parameters<F>
 ) {
-  return new _Timeout(callback, ms, false, args) as ITimeout
+  return new Timeout(callback, ms, false, args) as ITimeout
 }
 
 function longSetInterval<F extends (...a: any[]) => any>(
@@ -71,7 +79,7 @@ function longSetInterval<F extends (...a: any[]) => any>(
   ms?: number,
   ...args: Parameters<F>
 ) {
-  return new _Timeout(callback, ms, true, args) as ITimeout
+  return new Timeout(callback, ms, true, args) as ITimeout
 }
 
 export { longSetTimeout, longSetInterval }
